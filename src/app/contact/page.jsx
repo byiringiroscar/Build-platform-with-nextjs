11d02dba00d74bc9135506fda8ee308b09994598
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-16 21:48:39 +0200

    lazy loading

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const Contact = () => {
   return (
-    <div>page</div>
+    <div>Contact</div>
   )
 }
 
-export default page
+export default Contact
