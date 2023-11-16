7f43e30bb54ee2917c07e93daa286f215eb5b195
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-16 21:34:07 +0200

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+        <div>©2023 Lamamia. All rights reserved.</div>
+        <div></div>
+    </div>
   )
 }
 
