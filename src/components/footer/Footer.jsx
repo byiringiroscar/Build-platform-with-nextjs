bb81a16458b59c82719471ea58e4453b0d3dd498
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-24 00:16:22 +0200

    wip

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => {
   return (
     <div className={styles.container}>
         <div>©2023 Lamamia. All rights reserved.</div>
-        <div>
-          <Image src="/1.png" alt="Instagram" width={15} height={15} />
-          <Image src="/2.png" alt="Facebook" width={15} height={15} />
-          <Image src="/3.png" alt="Twitter" width={15} height={15} />
-          <Image src="/4.png" alt="YouTube" width={15} height={15} />
+        <div className={styles.social}>
+          <Image src="/1.png" alt="Instagram" className={styles.icon} width={15} height={15} />
+          <Image src="/2.png" alt="Facebook" className={styles.icon} width={15} height={15} />
+          <Image src="/3.png" alt="Twitter" className={styles.icon} width={15} height={15} />
+          <Image src="/4.png" alt="YouTube" className={styles.icon} width={15} height={15} />
         </div>
     </div>
   )
