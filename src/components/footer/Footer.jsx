91c9e196608f5775a5edf7aeef993c3761fc0aaa
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-24 00:08:59 +0200

    Wip for footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './page.module.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <div>©2023 Lamamia. All rights reserved.</div>
         <div></div>
     </div>
