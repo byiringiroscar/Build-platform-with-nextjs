f86e544acb354f2c4aa043b3aeb6ac0cbf952ae8
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-24 00:06:12 +0200

    Wip for nav

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,5 +1,7 @@
+"use client"
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
+import styles from './page.module.css';
 
 const links = [
     {
@@ -36,12 +38,19 @@ const links = [
 
 const NavBar = () => {
   return (
-    <div>
-        <Link href='/'>lamina</Link>
-        <div>
+    <div className={styles.container}>
+        <Link href='/' className={styles.logo}>lamina</Link>
+        <div className={styles.links}>
             {links.map(link => (
-                <Link key={link.id} href={link.url}>{link.title}</Link>
+                <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
             ))}
+            <button
+            className={styles.logout}
+            type='button'
+            onClick={() => {
+              console.log('logged out')
+            }}
+            >Logout</button>
         </div>
 
     </div>
