63395702769ef219d1f25e52548ca91da8b10fa7
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-24 05:46:31 +0200

    WIP

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,18 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from '../../public/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products. </h1>
+        <p className={styles.desc}>Turning your Idea into reality. we bring together the teams from the global tech industry</p>
+        <button className={styles.buttons}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt='hero' className='styles.img' />  
+      </div>
     </div>
   )
 }
