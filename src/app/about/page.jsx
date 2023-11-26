c16bf2b696eebbea6f126545aae68a0e64ca555e
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-26 11:22:42 +0200

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>about</div>
+    <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image src='' alt='' className={styles.img} />
+        </div>
+        <div className={styles.textContainer}></div>
+    </div>
   )
 }
 
