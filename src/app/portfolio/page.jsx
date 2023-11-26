339528937e106d636816fed58f8c28d530224d0b
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-26 20:43:47 +0200

    Update page.jsx

---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './page.module.css'
+
+const page = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default page
