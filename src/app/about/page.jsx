f29fc2bd29f52847d66695721a1035854c101c62
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-26 13:36:10 +0200

    wip

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,16 +7,16 @@ const page = () => {
   return (
     <div className={styles.container}>
         <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          fill={true}
-          alt="about"
-          className={styles.img}
-        />
-        <div className={styles.imgText}>
-          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
-          <h2 className={styles.imgDesc}>  Handcrafting award winning digital experiences </h2>
-        </div>
+          <Image
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            fill={true}
+            alt="about"
+            className={styles.img}
+          />
+          <div className={styles.imgText}>
+            <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+            <h2 className={styles.imgDesc}>  Handcrafting award winning digital experiences </h2>
+          </div>
         </div>
         <div className={styles.textContainer}>
         <div className={styles.item}>
