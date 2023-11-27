d85017b184bb7a3169157ad442023330bf6de737
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-27 09:54:23 +0200

    Get data dynamically

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,28 +2,43 @@ import React from 'react'
 import styles from './page.module.css'
 import Button from '@/components/Button/Button'
 import Image from 'next/image'
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
+
+
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
 
 const Category = ({params}) => {
-  const { category } = params
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
 
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text='SeeMore' url='#' />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill={true}
+              src={item.image}
+              alt=""
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-              <Image
-                className={styles.img}
-                fill={true}
-                src='https://images.pexels.com/photos/3401402/pexels-photo-3401402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                alt=''
-                />
-        </div>
-      </div>
+      ))}
 
     </div>
   )
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -13,7 +13,7 @@ const page = () => {
         <Link href="/portfolio/websites" className={styles.item}>
           <span className={styles.title}>Websites</span>
         </Link>
-        <Link href="/portfolio/application" className={styles.item}>
+        <Link href="/portfolio/applications" className={styles.item}>
           <span className={styles.title}>Application</span>
         </Link>
       </div>
