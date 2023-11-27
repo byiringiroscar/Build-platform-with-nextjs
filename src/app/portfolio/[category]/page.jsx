4bee7c10b0612bd4e467771d7c51c4acb4e6b3f4
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-27 09:48:03 +0200

    Update category portfolio

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import styles from './page.module.css'
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 
 const Category = ({params}) => {
   const { category } = params
-  console.log('-------', params)
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text='SeeMore' url='#' />
+        </div>
+        <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                fill={true}
+                src='https://images.pexels.com/photos/3401402/pexels-photo-3401402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt=''
+                />
+        </div>
+      </div>
+
     </div>
   )
 }
