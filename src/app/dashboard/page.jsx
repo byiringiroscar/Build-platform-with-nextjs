3ae24322eff8be562fa149e0a830a66d5bdee170
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-01 16:22:45 +0200

    Update page.jsx

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import React, { useEffect, useState } from 'react'
+
+const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData  = async () => {
+      setIsLoading(true)
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts', 
+      { cache: 'no-store' }
+      )
+     
+      if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        setErr(true)
+      }
+      const data = await res.json()
+      setData(data);
+      setIsLoading(false)
+    }
+    getData();
+
+  }, [])
+
+  console.log(data)
   return (
     <div>
-      
+       Dashboard
     </div>
   )
 }
 
-export default page
+export default Dashboard
