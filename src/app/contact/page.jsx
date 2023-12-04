8fdc4b6da571e0eecc6a34d0f6d44c0d273a4fdc
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-04 13:07:27 +0200

    Use metadata tochange title, desc to page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,11 @@ import styles from './page.module.css';
 import Image from 'next/image';
 import Button from '@/components/Button/Button';
 
+export const metadata = {
+  title: 'contact',
+  description: 'This is the contact page',
+}
+
 const Contact = () => {
   return (
     <div className={styles.container}>
