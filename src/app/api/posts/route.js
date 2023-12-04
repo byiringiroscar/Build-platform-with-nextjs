dce7a61482a16c361c30b3fa8773a05105dc89d2
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-04 11:58:39 +0200

    Connect to db and endpoint

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,5 +1,17 @@
 import { NextResponse } from "next/server"
+import connect from "@/utils/db";
+import Post from "@/models/Post";
 
 export const GET = async (request) =>{
+    try{
+        await connect();
+
+        const posts = await Post.find()
+        return new NextResponse(posts, { status: 200 })
+
+    }
+    catch(error){
+        return new NextResponse(error, { status: 500 })
+    }
     return new NextResponse("It works", { status: 200 })
-}
+} 
