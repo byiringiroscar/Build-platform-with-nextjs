ff6f627bbfb16f7cc0bbf3a7587d8e17caace744
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-05 13:28:44 +0200

    Setup button  for google auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,15 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+const handler =  NextAuth({
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       }),
   ],
-})
+});
+
+// export default handler;
 
 export { handler as GET, handler as POST };
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -36,10 +36,10 @@ const Dashboard = () => {
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
-  console.log(data)
-  if (status === "loading") {
-    return <p>Hang on there...</p>
-  }
+  // console.log(data)
+  // if (status === "loading") {
+  //   return <p>Hang on there...</p>
+  // }
   return (
     <div>
        Dashboard
