dede9674b15f4f820b4f669d61c4f2cf068d8208
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-07 13:23:31 +0200

    Update nextauth login in endpoint

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github"
-
-// export default NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      
-//     })
-//   ],
-// })
+import connect from "@/utils/db";
+import User from "@/models/User";
+import bcrypt from 'bcryptjs';
 
 const handler =  NextAuth({
   providers: [
@@ -18,6 +11,36 @@ const handler =  NextAuth({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       }),
+
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials){
+        await connect()
+        try{
+          const user = User.findOne({email: credentials.email})
+          if(user){
+            // check password
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if(isPasswordCorrect){
+              return user
+            }
+            else{
+              throw new Error('Password is incorrect')
+            }
+          }
+          else{
+            throw new Error('User not found')
+          }
+        }
+        catch(err){
+          throw new Error(err)
+        }
+      }
+    })
   ],
 });
 
