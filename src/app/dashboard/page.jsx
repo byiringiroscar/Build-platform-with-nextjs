8cfce3ab6a65759ba5f67191258398e901a407de
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-07 13:58:59 +0200

    Update page.jsx

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import useSWR from 'swr'
 import React, { useEffect, useState } from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/navigation'
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -30,21 +31,25 @@ const Dashboard = () => {
   // console.log(data)
   // const { data: session, status } = useSession()
   // const userEmail = session?.user?.email
-
-  const session  = useSession();
-  console.log(session)
+  
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
-
-  // console.log(data)
-  // if (status === "loading") {
-  //   return <p>Hang on there...</p>
-  // }
-  return (
-    <div>
-       Dashboard
-    </div>
-  )
+  const session  = useSession();
+  const router = useRouter()
+  if(session.status === 'loading'){
+    return <p>Hang on there...</p>
+  }
+  if(session.status === 'unauthenticated'){
+    router?.push("/dashboard/login")
+  }
+  console.log(session)
+  if(session.status === 'authenticated'){
+    return (
+      <div>
+         Dashboard
+      </div>
+    )
+  }
 }
 
 export default Dashboard
