c0f9de8a0277a6c35523a366cadf4deac815c4b9
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-07 12:13:39 +0200

    Update page.jsx

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Username"
+          required
+          className={styles.input}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+        {error && "Something went wrong!"}
+      </form>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/login">
+        Login with an existing account
+      </Link>
+    </div>
   )
 }
 
