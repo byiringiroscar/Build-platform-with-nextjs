cf4812eab2ddc9195a3d788d4f855bbd2c66dc08
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-07 12:35:34 +0200

    Update register page

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Link from 'next/link'
 import styles from './page.module.css'
+import { useRouter } from 'next/navigation'
 
 const Register = () => {
+  const [err, setErr] = useState(false);
+  const router = useRouter()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    try{
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name, email, password})
+      });
+
+      res.status === 201 && router.push('/dashboard/login?success=Account has been created');
+  
+    }
+    catch(err){
+      setErr(true)
+    }
+  }
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Create an Account</h1>
       <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <input
           type="text"
           placeholder="Username"
@@ -27,7 +54,7 @@ const Register = () => {
           className={styles.input}
         />
         <button className={styles.button}>Register</button>
-        {/* {error && "Something went wrong!"} */}
+        {err && "Something went wrong!"}
       </form>
       <span className={styles.or}>- OR -</span>
       <Link className={styles.link} href="/dashboard/login">
