89b8bec75de177cfdc7ef7cd5c0711c0d2cbe596
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-07 12:12:47 +0200

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -17,13 +17,6 @@ const handler =  NextAuth({
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        // authorization: {
-        //   params: {
-        //     prompt: "consent",
-        //     access_type: "offline",
-        //     response_type: "code"
-        //   }
-        // }
       }),
   ],
 });
